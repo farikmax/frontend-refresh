9e6a5b47d69b653772c94991ea624a3827b0271d
farikmax <farikmax@gmail.com>
2026-06-03 20:24:55 +0300

    Complete JS core: fetch, filter, and map with destructuring

--- script.js
+++ script.js
@@ -46,7 +46,11 @@ async function getUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
-    console.log(data);
+    const info = data
+      .filter((el) => el.id < 6)
+      .map(({ name, email }) => `Пользователь: ${name}, Email: ${email}`);
+
+    console.log(info);
   } catch (error) {
     console.error("Произошла ошибка при загрузке данных:", error.message);
   }
